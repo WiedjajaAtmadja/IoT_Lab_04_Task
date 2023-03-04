7bc4f7c7c88f8d68598c37254c08dd78cc5268f4
Steffanus W <swiedjaja@gmail.com>
2023-03-04 16:17:17 +0700

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#define LED_GREEN  4
+#define LED_YELLOW 5
+#define LED_RED    18
+
+void taskBlinkRed(void *pvParameters) {
+  while (1) {
+    Serial.println("taskBlinkRed");
+    digitalWrite(LED_RED, HIGH);
+    vTaskDelay(200 / portTICK_PERIOD_MS);
+    digitalWrite(LED_RED, LOW);
+    vTaskDelay(200 / portTICK_PERIOD_MS);
+  }
+}
+
+void taskBlinkGreen(void *pvParameters) {
+  while (1) {
+    Serial.println("taskBlinkGreen");
+    digitalWrite(LED_GREEN, HIGH);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    digitalWrite(LED_GREEN, LOW);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+    // init Leds
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  digitalWrite(LED_GREEN, LOW);
+  digitalWrite(LED_YELLOW, LOW);
+  digitalWrite(LED_RED, LOW);
+
+  xTaskCreatePinnedToCore(taskBlinkRed, "taskBlinkRed", 2048, NULL, 1, NULL, 0);
+  xTaskCreatePinnedToCore(taskBlinkGreen, "taskBlinkGreen", 2048, NULL, 1, NULL, 0);
+  Serial.println("System ready");
+}
+
+void loop() {
+  Serial.println("Main task");
+  digitalWrite(LED_BUILTIN, HIGH);
+  vTaskDelay(50 / portTICK_PERIOD_MS);
+  digitalWrite(LED_BUILTIN, LOW);  
+  vTaskDelay(1000);
+}
